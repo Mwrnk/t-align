d3f030a5fcf7a9eb0bce5fa5eea36304e066fef5
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2025-03-04 11:39:36 -0300

    Initial commit: Set up React + Tailwind task manager

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-blue-600">Task Manager</h1>
+      <p className="mt-2 text-lg">Developed by Mateus Werneck</p>
+    </div>
+  );
+}
+
+export default App;
