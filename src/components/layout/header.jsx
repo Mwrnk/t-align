3896f3136bebb273e513340b7bc5800599f6bec0
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2025-03-06 01:11:22 -0300

    style: integrate custom fonts (GolosText, DelaGothicOne) into CSS

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -4,7 +4,7 @@ import { Button, Dropdown, Input } from '../ui';
 const Header = () => {
   return (
     <header className="flex p-3 justify-between items-center self-strech">
-      <h1>T-ALIGN</h1>
+      <h1 className="font-title">T-ALIGN</h1>
 
       <div className="flex w-auto p-0.75 items-center gap-3">
         <Input placeholder={'Search Tasks...'} />
