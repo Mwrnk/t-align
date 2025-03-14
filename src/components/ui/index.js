060f438766cf8003203cd95c710e20b86afa5bf0
Mateus Werneck <mateuswerneckelizeu@yahoo.com.br>
2025-03-14 01:21:04 -0300

    Add dropdown-menu component and fix color variables for warning and success

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,134 @@
 import TaskSection from './taskSection';
 import TaskCard from './taskCard';
 import TaskForm from './taskForm';
+import DeleteTaskDialog from './DeleteTaskDialog';
+import ExportTasksDialog from './ExportTasksDialog';
+import { DatePicker } from './date-picker';
 
-export { TaskSection, TaskCard, TaskForm };
+// Importações dos componentes shadcn/ui
+import { Avatar, AvatarFallback, AvatarImage } from './avatar';
+import { Button } from './button';
+import { Calendar } from './calendar';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './card';
+import { Checkbox } from './checkbox';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from './dialog';
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from './form';
+import { Input } from './input';
+import { Label } from './label';
+import { Popover, PopoverContent, PopoverTrigger } from './popover';
+import { RadioGroup, RadioGroupItem } from './radio-group';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './select';
+import { Switch } from './switch';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './tabs';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './tooltip';
+import { Toaster } from './sonner';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from './alert-dialog';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger
+} from './dropdown-menu';
+
+export { 
+  // Componentes personalizados
+  TaskSection, 
+  TaskCard, 
+  TaskForm,
+  DeleteTaskDialog,
+  ExportTasksDialog,
+  DatePicker,
+  
+  // Componentes shadcn/ui
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Avatar, 
+  AvatarFallback, 
+  AvatarImage,
+  Button,
+  Calendar,
+  Card, 
+  CardContent, 
+  CardDescription, 
+  CardFooter, 
+  CardHeader, 
+  CardTitle,
+  Checkbox,
+  Dialog, 
+  DialogContent, 
+  DialogDescription, 
+  DialogFooter, 
+  DialogHeader, 
+  DialogTitle, 
+  DialogTrigger,
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+  Form, 
+  FormControl, 
+  FormDescription, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage,
+  Input,
+  Label,
+  Popover, 
+  PopoverContent, 
+  PopoverTrigger,
+  RadioGroup,
+  RadioGroupItem,
+  Select, 
+  SelectContent, 
+  SelectItem, 
+  SelectTrigger, 
+  SelectValue,
+  Switch,
+  Tabs, 
+  TabsContent, 
+  TabsList, 
+  TabsTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+  Toaster
+};
